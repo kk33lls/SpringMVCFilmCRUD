5d2eca0410d2985a250832576c40fd428ae49efa
Jinyoung Yoon <jyoo7375@gmail.com>
2025-04-25 19:42:55 -0400

    Create FilmDAO, add Actor class

---
+++ src/main/java/com/skilldistillery/film/data/FilmDAO.java
@@ -0,0 +1,17 @@
+package com.skilldistillery.film.data;
+
+import java.util.List;
+
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Film;
+
+public interface FilmDAO {
+  public Film findFilmById(int filmId);
+  public Actor findActorById(int actorId);
+  public List<Actor> findActorsByFilmId(int filmId);
+  public List<Film> findFilmByKeyword(String keyword);
+  
+  Film addFilm(Film newFilm);
+  Film updateFilm(Film updatingFilm);
+  void deleteFilm(Film filmToDelete);
+}
