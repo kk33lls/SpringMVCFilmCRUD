dd925be11ac651ae061bd9c4a31c8ba60fa84cfd
Kirstin <kirstin.eells98@gmail.com>
2025-04-25 18:24:27 -0600

    500 error fix

--- src/main/java/com/skilldistillery/film/data/FilmDaojdbcimpl.java
+++ src/main/java/com/skilldistillery/film/data/FilmDaojdbcimpl.java
@@ -2,9 +2,11 @@ package com.skilldistillery.film.data;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.skilldistillery.film.entities.Actor;
 import com.skilldistillery.film.entities.Film;
-
+@Component
 public class FilmDaojdbcimpl implements FilmDAO {
 
 	@Override
